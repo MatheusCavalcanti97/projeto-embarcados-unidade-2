c1b9bc60e20bfada4eea07d8beb084fbe0c03db6
matheuscavalcanti97 <matheus_cla@hotmail.com>
2025-08-11 00:43:08 -0300

    feat: inserção topico para controle automatizado do limiar quando referente ao modo automatico + rota de verificação do limiar atual.

--- api/src/routes/lampRoutes.js
+++ api/src/routes/lampRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   manualControl,
   setMode,
   getStatus,
+  getLimiar,
+  setLimiar,
 } = require("../controller/lampController");
 
 router.post("/manual", manualControl);
 router.post("/modo", setMode);
 router.get("/status", getStatus);
+router.get("/limiar", getLimiar);
+router.post("/limiar", setLimiar);
 
 module.exports = router;
